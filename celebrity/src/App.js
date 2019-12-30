48fcd14f4d5d88bd2c0a6506c15151fbac1fadeb
RobertElias <robert.elias@live.com>
2019-12-30 15:24:42 -0800

    Updates to Timer.js

--- celebrity/src/App.js
+++ celebrity/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import React from 'react';
 import Login from './components/Login';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>HELLO CELEBRITY!!</h1>
+        <Timer></Timer>
         <Login></Login>
       </header>
     </div>
--- celebrity/src/components/Timer.js
+++ celebrity/src/components/Timer.js
@@ -1,2 +1,50 @@
 //Sets timer to 20's and adjust
 //for difficulty level on stretch
+import React, { useState, useEffect } from 'react';
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(20);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds - 1);
+      }, 2000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  return (
+    <div className='app'>
+      <div className='time'>{seconds}s</div>
+      <div className='row'>
+        <button
+          className={`button button-primary button-primary-${
+            isActive ? 'active' : 'inactive'
+          }`}
+          onClick={toggle}
+        >
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button className='button' onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
