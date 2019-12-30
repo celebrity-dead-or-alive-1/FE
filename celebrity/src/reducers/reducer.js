819c8bebde224c896e8671da29111fec247ffe7a
Gabriel Anguiano <anguianog97@gmail.com>
2019-12-30 14:35:27 -0800

    basic redux done

---
+++ celebrity/src/reducers/reducer.js
@@ -0,0 +1,15 @@
+import { CELEB_DATA_FETCHING, CELEB_DATA_SUCCESS, CELEB_DATA_FAILURE } from '../actions/actions'
+
+const initialState = {
+    celebs: [],
+    isFetching: false,
+    error: ''
+}
+
+
+export const reducer = (state = initialState, action) => {
+            switch(action.type) {
+                default :
+                return state 
+        } 
+} 
