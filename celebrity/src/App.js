84bb24cb438bc72aeda8651440d975872275c5f3
RobertElias <robert.elias@live.com>
2019-12-30 14:50:00 -0800

    Updated login form

--- celebrity/src/App.js
+++ celebrity/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
 import React from 'react';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>HELLO CELEBRITY!!</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>HELLO CELEBRITY!!</h1>
+        <Login></Login>
       </header>
     </div>
   );
--- celebrity/src/components/Login.js
+++ celebrity/src/components/Login.js
@@ -1,1 +1,60 @@
 //User logins to come back and play the game again
+
+import React, { useState } from 'react';
+
+const Login = props => {
+  const [note, setNote] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChanges = e => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+    console.log(e.target.value);
+  };
+
+  const submitForm = e => {
+    e.preventDefault();
+    props.addNewNote(note);
+    setNote({
+      name: '',
+      email: '',
+      password: ''
+    });
+  };
+  return (
+    <form onSubmit={submitForm}>
+      <label htmlFor='name'>Name:</label>
+      <input
+        id='name'
+        type='text'
+        name='name'
+        value={note.name}
+        onChange={handleChanges}
+      />
+
+      <label htmlFor='email'>Email:</label>
+      <input
+        id='email'
+        type='text'
+        name='email'
+        value={note.email}
+        onChange={handleChanges}
+      />
+
+      <label htmlFor='password'>Password:</label>
+      <input
+        id='password'
+        type='text'
+        name='password'
+        value={note.password}
+        onChange={handleChanges}
+      />
+
+      <button type='submit'>Login</button>
+    </form>
+  );
+};
+
+export default Login;
