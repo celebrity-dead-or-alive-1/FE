a815faf17633f8562c7d5307508e6c2113cd0cb5
RobertElias <robert.elias@live.com>
2019-12-30 16:24:19 -0800

    Routes on App.js

--- celebrity/src/App.js
+++ celebrity/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import React from 'react';
 import Login from './components/Login';
 import Timer from './components/Timer';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>HELLO CELEBRITY!!</h1>
-        <Timer></Timer>
-        <Login></Login>
+        <Route exact path='/Login' />
+        <Route exact path='/Timer' />
       </header>
     </div>
   );
