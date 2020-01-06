b6de97c68617d33b3aa8bbce8db60bea370ea5dd
RobertElias <robert.elias@live.com>
2020-01-06 13:50:17 -0800

    updates to Timer.js App.js

--- celebrity/src/App.js
+++ celebrity/src/App.js
@@ -3,55 +3,34 @@ import './App.css';
 import React from 'react';
 
 import Register from './components/Register';
-<<<<<<< HEAD
 import { Route, NavLink } from 'react-router-dom';
 import Admin from './components/Admin';
 import Login from './components/Login';
 import MyTimer from './components/Timer';
 import LandingPage from './components/LandingPage';
+import Score from './components/Score';
+import { PrivateRoute } from './components/PrivateRoutes';
+import AdminIndiv from './components/AdminIndiv';
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <NavLink to='/Admin'>Admin</NavLink>
         <NavLink to='/Register'>Register</NavLink>
-        <NavLink to='/Login'>Login</NavLink>
-        <NavLink to='/Timer'>Timer</NavLink>
+        <NavLink to='/login'>Log In</NavLink>
         <Route exact path='/' component={LandingPage} />
         <Route exact path='/Admin' component={Admin} />
         <Route exact path='/Register' component={Register} />
         <Route exact path='/Login' component={Login} />
-        <Route exact path='/Timer' component={MyTimer} />
+
+        <PrivateRoute exact path='/Scores' component={Score} />
+        <Route
+          path='/Admin/AdminIndividual/:id'
+          render={props => {
+            return <AdminIndiv {...props} />;
+          }}
+        />
         <MyTimer />
-=======
-import { Route, NavLink } from 'react-router-dom'
-import Admin from './components/Admin'
-import Login from './components/Login';
-import Timer from './components/Timer';
-import LandingPage from './components/LandingPage';
-import Score from './components/Score';
-import { PrivateRoute } from './components/PrivateRoutes'
-import AdminIndiv from './components/AdminIndiv'
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <NavLink to='/Admin'>Admin</NavLink>
-        <NavLink to='/Register'>Register</NavLink>
-        <NavLink to='/login'>Log In</NavLink>
-        <Route exact path='/' component={LandingPage}/>
-        <Route exact path ='/Admin' component ={Admin}/>
-        <Route exact path='/Register' component={Register}/>
-        <Route exact path='/Login' component={Login}/>
-        <Route exact path='/Timer' component={Timer}/>
-        <PrivateRoute  exact path='/Scores' component={Score}/>
-        <Route path ='/Admin/AdminIndividual/:id' render={props => {
-          return <AdminIndiv {...props}/> 
-        }}
-       />
-
-
->>>>>>> ee2d6f4cb469216421eda5040c728d0a67cd536e
       </header>
     </div>
   );
--- celebrity/src/components/LandingPage.js
+++ celebrity/src/components/LandingPage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 const LandingPage = () => {
@@ -10,16 +9,3 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
-=======
-import React from 'react'
-
-const LandingPage = () => {
-    return(
-        <div>
-            <h1>Hello Celebrity!!!</h1>
-        </div>
-    )
-}
-
-export default LandingPage
->>>>>>> ee2d6f4cb469216421eda5040c728d0a67cd536e
